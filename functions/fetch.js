6aa2da4f36aeff14547d1f1b49392916f3890677
Arvin How <arvinhow95@gmail.com>
2023-10-06 04:28:47 +0800

    added area-requirement

--- functions/fetch.js
+++ functions/fetch.js
@@ -1,22 +1,54 @@
+const { google } = require('googleapis')
 const fs = require('fs')
 require('dotenv').config();
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
 
-const comp_id = process.env.AIRTABLE_TABLE_ID_COMP
-const pset_id = process.env.AIRTABLE_TABLE_ID_PSET
-const baseID = process.env.AIRTABLE_BASEID
-const AIRTABLE_TOKEN = process.env.AIRTABLE_TOKEN
 
-async function get() {
-    const resp = await fetch(`${baseID}/${comp_id}`, {
-        headers: {
-            "Authorization": `Bearer ${AIRTABLE_TOKEN}`
-        }
+async function main() {
+    const sheets = google.sheets('v4');
+    const SPREADSHEET_ID = '14_9NvXR8FbBpSI6w6s3lCjtmY_9bGG4dw4Tdzn4kPfc';
+
+    // `Overview!B3:F12`,
+    const rows = await sheets.spreadsheets.values.get({
+        auth: GOOGLE_API_KEY,
+        spreadsheetId: SPREADSHEET_ID,
+        range: 'Overview!B3:F12',
+        majorDimension: "COLUMNS"
+    }).then(res => {
+        return res.data.values
     })
 
-    const res = await resp.json()
+    const data = []
 
-    console.log(res);
-    fs.writeFileSync('test.json', JSON.stringify(res, null, 2))
+    for (const arr of rows) {
+        const props = []
+        arr.slice(2).forEach(propString => {
+            if (!propString.trim()) {
+                return
+            }
+
+            const regex = /(.+?)\s?\[(.+?)\]/
+            let [PropertyName, DataType] = propString.match(regex)?.slice(1) || []
+
+            if (DataType == 'Text') {
+                DataType = 'Label'
+            }
+
+            props.push({ PropertyName, DataType })
+
+        })
+
+        data.push({
+            SubType: arr[0],
+            PropertySet: arr[1],
+            Properties: props
+        })
+    }
+
+
+
+    fs.writeFileSync('./test.json', JSON.stringify(data, null, 2))
 }
 
-get()
+
+main()
---
+++ src/routes/(main)/area-requirement/+layout.svelte
@@ -0,0 +1,15 @@
+<div class="container">
+    <slot />
+</div>
+
+<style lang="scss">
+.container {
+    grid-column: 1/-1;
+    width: 1100px;
+    margin-inline: auto;
+    @media screen and (max-width: $mobile) {
+        width: 100%;
+        padding-inline: 1rem;
+    }
+}
+</style>
