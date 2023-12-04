8416b989316dae6fc6904a8a7492a252ccd9dfd1
arvinhow95@gmail.com <arvinhow95@gmail.com>
2023-12-04 10:21:00 +0800

    tmep

---
+++ src/routes/(main)/identified-component/[IdentifiedComponent]/Mg.svelte
@@ -0,0 +1,13 @@
+<script>
+import ModellingGuide from "./ModellingGuide.svelte";
+export let identifiedComponent;
+let isEditing = false;
+let editor;
+</script>
+
+<ModellingGuide
+    bind:this={editor}
+    IdentifiedComponent={mg_data.IdentifiedComponent}
+    on:save={() => {
+        isEditing = false;
+    }} />
